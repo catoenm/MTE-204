6ec7db55621339d246d285817acd6a1dddce6130
devonkcc <devonkcc@gmail.com>
2016-05-11 20:47:39 -0400

    Implementing process material props

--- MATLAB/get_DOF.m
+++ MATLAB/get_DOF.m
@@ -1,9 +1,8 @@
 function [DOF] = get_DOF(NODES)
-
 % % % This function receives the nodes matrix.
 % % % It determines and returns the degree of freedom of the problem.
 % % % I.e. Is the problem a 1D problem? 2D problem? 3D problem?
 
-DOF = size(NODES, 2);
+    DOF = size(NODES, 2);
 
 end
--- MATLAB/process_material_props.m
+++ MATLAB/process_material_props.m
@@ -2,5 +2,7 @@ function [AREA, YOUNG] = process_material_props(PROPS)
 % % % This function receives the material properties matrix
 % % % This function partitions and returns the area and elastic modulus
 
+    AREA = PROPS(:,1);
+    YOUNG = PROPS(:,2);
 
 end
