3f540274707bebc7740d72dd34cf69f88bab18a4
Mitchell Catoen <mitchellcatoen@gmail.com>
2016-05-11 20:31:51 -0400

    Rearranged file structure and added get_dof function

--- MATLAB/get_DOF.m
+++ MATLAB/get_DOF.m
@@ -4,6 +4,6 @@ function [DOF] = get_DOF(NODES)
 % % % It determines and returns the degree of freedom of the problem.
 % % % I.e. Is the problem a 1D problem? 2D problem? 3D problem?
 
-
+DOF = size(NODES, 2);
 
 end
