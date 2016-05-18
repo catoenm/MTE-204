92e1da3c8eeb3dd0b6e250bb3edeac058c48b750
Danyon <danyonchu.21@gmail.com>
2016-05-18 07:17:30 -0400

    recent changes to writedata out function with updated header and name files

--- MATLAB/writedataout.m
+++ MATLAB/writedataout.m
@@ -16,15 +16,120 @@ end
 
 fid2 = fopen('./' + filename + '.txt','w');
 fprintf(fid1, '%s\n', nFile);
-fprintf(fid1, '\n%s\n%s\n\%s\n\%s\n',hFile{1}, hFile{2}, hFile{3}, hFile{4});
 
-sizeFR = size(1,FGLOBAL)
-sizeFC = size(FGLOBAL,1)
+k = strfind(filename,'1a');
+fprintf(fid1, '\n%s\n', hFile{1});
+if(isempty(k))
+    fprintf(fid1, '%s\n', hFile{3});
+else
+    fprintf(fid1, '%s\n', hFile{2});
+end
 
-for ii = 1:sizeFR
-    fprintf(fid2,'%10.5f\n',X(ii)); %file id, %f the location of the value
+fprintf(fid1, '\%s\n\%s\n', hFile{4}, hFile{5});
+
+nRows = size(FGLOBAL, 1); 
+%nRows should be same for following matrices: 
+%FGLOBAL, UGLOBAL, NODES
+for ii = 1:nRows
+    fprintf(fid2,'F%c = %10.5f',32+ii,FGLOBAL(ii)); %file id, %f the location of the value
+    %assumes less than 26 values
     if (mod(ii,2)==0)
         fprintf(fid2,'\n');
     else
-        fprintf(fid2,',');
+        fprintf(fid2,', ');
+    end
 end
+
+fprintf(fid1, '\n%s\n', hFile{6});
+
+for ii = 1:nRows
+    fprintf(fid2,'U');
+    %assumes at most 3DOF
+    if DOF == 1
+        fprintf(fid2,'%d',ii);   
+    elseif DOF == 2
+        if mod(ii,2)== 0
+            fprintf(fid2,'%dy',ii); 
+        else
+        	fprintf(fid2,'%dx',ii);   
+        end   
+    else
+        if mod(ii,3)== 0
+            fprintf(fid2,'%dz',ii); 
+        elseif mod(ii,3)== 2
+        	fprintf(fid2,'%dy',ii); 
+        else
+            fprintf(fid2,'%dx',ii); 
+        end
+    end
+    
+    fprintf(fid2,'%10.5f',UGLOBAL(ii)); %file id, %f the location of the value
+
+    if (mod(ii,2)==0)
+        fprintf(fid2,'\n');
+    else
+        fprintf(fid2,', ');
+    end
+end
+
+fprintf(fid1, '\n%s\n', hFile{7});
+
+
+for ii = 1:nRows
+    fprintf(fid2,'F');
+    %assumes at most 3DOF
+    if DOF == 1
+        fprintf(fid2,'%d',ii);   
+    elseif DOF == 2
+        if mod(ii,2)== 0
+            fprintf(fid2,'%dy',ii); 
+        else
+        	fprintf(fid2,'%dx',ii);   
+        end   
+    else
+        if mod(ii,3)== 0
+            fprintf(fid2,'%dz',ii); 
+        elseif mod(ii,3)== 2
+        	fprintf(fid2,'%dy',ii); 
+        else
+            fprintf(fid2,'%dx',ii); 
+        end
+    end
+    
+    fprintf(fid2,'%10.5f',NODES(ii)); %file id, %f the location of the value
+    
+    if (mod(ii,2)== 0)
+        fprintf(fid2,'\n');
+    else
+        fprintf(fid2,', ');
+    end
+end
+
+fprintf(fid2, '\n%s\n%s\n%s\n', hFile{7}, hFile{8}, hFile{9});
+
+nCol = sizeof(SCTR,2);
+for ii = 1:nRows
+    fprintf(fid2,'%d, ',ii);
+    for jj = 1:nCol
+        fprintf(fid2,'%d, ',ii,SCTR(ii,jj));
+    end
+    fprintf(fid2,'%f',abs(STRESS(ii)));
+    
+    if STRESS(ii) < 0 
+        fprintf(fid2,'[Compression]');
+    else
+        fprintf(fid2,'[Tension]');  
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+    
