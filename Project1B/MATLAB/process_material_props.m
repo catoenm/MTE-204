6c72572e58dd8b4e221aff952121d4f69e7c2c17
Mitchell Catoen <mitchellcatoen@gmail.com>
2016-06-08 22:25:24 -0400

    changed file format

--- Project1B/MATLAB/process_material_props.m
+++ Project1B/MATLAB/process_material_props.m
@@ -1,10 +1,10 @@
-function [AREA, YOUNG, DAMPING, MASS] = process_material_props(PROPS)
+function [AREA, YOUNG, DAMPING, DENSITY] = process_material_props(PROPS)
 % % % This function receives the material properties matrix
 % % % This function partitions and returns the area and elastic modulus
 
-    AREA = PROPS(:,1);
-    YOUNG = PROPS(:,2);
-    DAMPING = PROPS(:,3);
-    MASS = PROPS(:,4);
+    YOUNG = PROPS(:,1);
+    AREA = PROPS(:,2);
+    DENSITY = PROPS(:,3);
+    DAMPING = PROPS(:,4);
 
 end
