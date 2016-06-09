6f5e6926fe140eef2d6363ff93ff3843f9a89598
David Ferris <david.j.ferris.21@gmail.com>
2016-06-08 22:36:17 -0400

    semicolo

--- Project1B/MATLAB/buildMGLOBAL.m
+++ Project1B/MATLAB/buildMGLOBAL.m
@@ -1,7 +1,7 @@
 function [ MGLOBAL ] = buildMGLOBAL(AREA,DENSITY,DOF,MGLOBAL,GPROPS)
 %buildGlobalMass
 %   Handle global mass matrix creation
-    MASS = AREA.*DENSITY.*GPROPS(:,3)
+    MASS = AREA.*DENSITY.*GPROPS(:,3);
     for i = 0:1:size(MASS)
         for j = 1:DOF
             MGLOBAL(i*DOF+j,i*DOF+j) = MASS(i+1);
