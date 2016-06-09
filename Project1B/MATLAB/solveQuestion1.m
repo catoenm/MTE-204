356f5959c81db1b10fd914c8ae157e7530dda3d7
devonkcc <devonkcc@gmail.com>
2016-06-09 17:38:37 -0400

    new solve 1

--- Project1B/MATLAB/solveQuestion1.m
+++ Project1B/MATLAB/solveQuestion1.m
@@ -2,6 +2,9 @@ clc
 clear all
 close all
 
+symbolSpec = '^*o.';
+colorSpec = 'rbmk';
+
 loadCurves = 'Input_Files/Question1/curves/';
 fileType = '*.txt';
 loadCurveFiles = dir(strcat(loadCurves,fileType));
@@ -9,7 +12,20 @@ props = 'Input_Files/Question1/props_1.txt';
 nodes = 'Input_Files/Question1/nodes_1.txt';
 sctr = 'Input_Files/Question1/sctr_1.txt';
 
+option = 2;
+
 for i = 1:length(loadCurveFiles)
-    disp(loadCurveFiles(i).name)
-    [uGlobal, fGlobal] = runSimulation(nodes, sctr, props, strcat(loadCurves, loadCurveFiles(i).name), 2)
+    results(i).option = option;
+    results(i).timeStep = loadCurveFiles(i).name(8:end);
+    [results(i).uGlobal, results(i).fGlobal] = runSimulation(nodes, sctr, props, strcat(loadCurves, loadCurveFiles(i).name), option);
 end
+
+figure;
+hold on;
+for i = 1:length(results)
+    plot(i,results(i).uGlobal(2), strcat(colorSpec(i),symbolSpec(i)),'DisplayName',results(i).timeStep);
+end
+
+legend('show');
+title('Question 1: Displacement of Node 2 Using Explict Dynamics');
+
