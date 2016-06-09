d006f346544ec16b48a896284b9aa5b6583d4922
devonkcc <devonkcc@gmail.com>
2016-06-09 16:19:27 -0400

    Deleted unused functions in 1B

--- Project1B/MATLAB/get_DOF.m
+++
@@ -1,9 +0,0 @@
-function [DOF] = get_DOF(NODES)
-% % % This function receives the nodes matrix.
-% % % It determines and returns the degree of freedom of the problem.
-% % % I.e. Is the problem a 1D problem? 2D problem? 3D problem?
-
-
-    DOF = size(NODES, 2); % size returns row , column, so return value of "column"
-
-end
--- Project1B/MATLAB/initialize_displacements.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = initialize_displacements( input_args )
-%initialize_displacements
-%   Create a function to initialize the displacements for all time steps.
-
-
-end
-
