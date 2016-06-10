eaa977d0d3c84e12497be2b5d9a6a7104582cf40
devonkcc <devonkcc@gmail.com>
2016-06-10 17:02:24 -0400

    Updates to load curve generator

--- Project1B/MATLAB/test.m
+++ Project1B/MATLAB/test.m
@@ -1,7 +1,7 @@
 clc
 clear all
 
-props = 'Input_Files/Question3/props.txt';
+props = 'Input_Files/Question3/props_3.txt';
 nodes = 'Input_Files/Question3/nodes_3.txt';
 sctr = 'Input_Files/Question3/sctr_3.txt';
 load_curve = 'Input_Files/Question3/curves/q3_freq_0.1.txt';
@@ -10,15 +10,15 @@ load_curve = 'Input_Files/Question3/curves/q3_freq_0.1.txt';
 
 nodesMatrix = load(nodes);
 sctrMatrix = load(sctr);
-% 
-% for i = 1:5000:length(uComplete)
-%     t = i*0.01;
-%     postprocesser(nodesMatrix(:,1),nodesMatrix(:,2),sctrMatrix',uComplete(:,i))   
-%     plottitle = sprintf('Deformation of Structure at Time %f ms' ,t);
-%     title(plottitle)
-%     plottitle = sprintf('Deformation of Structure at inc %d',i);
-%     saveas(2,plottitle,'png');
-%     display(t);
-%     clf(2);   
-% end
-% clc;
+
+for i = 1:5000:length(uComplete)
+    t = i*0.01;
+    postprocesser(nodesMatrix(:,1),nodesMatrix(:,2),sctrMatrix',uComplete(:,i))   
+    plottitle = sprintf('Deformation of Structure at Time %f ms' ,t);
+    title(plottitle)
+    plottitle = sprintf('Deformation of Structure at inc %d',i);
+    saveas(2,plottitle,'png');
+    display(t);
+    clf(2);   
+end
+clc;
