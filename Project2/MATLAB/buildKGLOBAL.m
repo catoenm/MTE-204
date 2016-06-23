af99297ec42d4db6db93cbd2f9eb9e03caff2ac0
devonkcc <devonkcc@gmail.com>
2016-06-23 17:02:10 -0400

    Project3 Initial Commit

---
+++ Project2/MATLAB/buildKGLOBAL.m
@@ -0,0 +1,95 @@
+function [KGLOBAL] = buildKGLOBAL(NODES,SCTR,DOF,YOUNG,AREA,KGLOBAL)
+
+
+    % % % This function receives the node list, the elemental connectivity
+    % matrix, degree of freedom of the problem, elastic modulus, area and an
+    % empty Kglobal matrix.
+
+    % % % This function builds the transformed local elemental matrix and inserts it into
+    % % % the global stiffness matrix.
+
+    % % % At the end of the function, the fully assembled Kglobal matrix is
+    % % % returned.
+
+    sctr_size = size(SCTR, 1); % # of elements/rows in SCTR
+
+    % initializing in properties matrix
+    geometric_props = zeros(sctr_size, 2); 
+
+    %Calculating properties
+    for i = 1:sctr_size % each element
+        index1 = SCTR(i, 1); % index of 1st node 
+        index2 = SCTR(i, 2); % index of 2nd node
+        delx = NODES(index1, 1) - NODES(index2, 1); % get x delta
+        if (DOF > 1)
+            dely = NODES(index1, 2) - NODES(index2, 2); % get y delta
+            length = sqrt((delx)^2 + (dely)^2); % calculate pythagorean length
+            angle  = atan(dely/delx); % calculate angle
+        else 
+            length = delx; 
+            angle = 0; 
+        end
+        
+        geometric_props(i, 1) = YOUNG(i,1) * AREA(i,1) / length; % E *A / L <= formula to get stiffness of a material
+        geometric_props(i, 2) = angle; % add angle to properties
+    end
+
+    %One dimensional case
+    if DOF == 1  
+        A = [1 -1; -1 1]; % transformation
+        for el = 1:sctr_size % each element
+            KLOCAL = geometric_props(el,1)*A; % get stiffness for this local kmatrix from property matrix
+            for i = 1:2
+                for j = 1:2
+                    index1 = SCTR(el,i); 
+                    index2 = SCTR(el,j);
+                    KGLOBAL(index1, index2) = KGLOBAL(index1, index2) + KLOCAL(i,j); % superposition the klocal value onto global Kmatrix
+                end
+            end
+        end
+    end
+
+    %Two dimensional case
+    if DOF == 2
+        for el = 1:sctr_size
+           %Keff values
+           theta = geometric_props(el, 2); % angle between the local axis and the global axis
+           stiffness = geometric_props(el, 1); % stiffness of the current element
+           a = cos(theta); % a is the consine of the angle 
+           b = sin(theta); % b " " sine
+           a2 = a^2 * stiffness;
+           b2 = b^2 * stiffness;
+           ab = a*b * stiffness;
+           
+
+           % kglobal = [ Kp Kn 
+           %             Kn Kp ]
+           Kp = [a2 ab;  % positive partitions of K-eff  
+                 ab b2];
+           Kn = -1*Kp; % negative partition
+           
+           for i = 1:2
+               for j = 1:2
+                   index1 = (SCTR(el, i)*2)-1; % getting indices of node with respect to global matrix
+                   index2 = (SCTR(el, j)*2)-1;
+                   if i == j
+                      Keff = Kp; 
+                   else
+                      Keff = Kn;
+                   end
+                   KGLOBAL(index1, index2)     = KGLOBAL(index1, index2) + Keff(1, 1); % superpositioning force onto index in global kmatrix
+                   KGLOBAL(index1, index2+1)   = KGLOBAL(index1, index2+1) + Keff(1, 2); %
+                   KGLOBAL(index1+1, index2)   = KGLOBAL(index1+1, index2) + Keff(2, 1); %
+                   KGLOBAL(index1+1, index2+1) = KGLOBAL(index1+1, index2+1) + Keff(2, 2); %
+               end
+           end
+        end
+    end
+
+    %Three dimensional case
+    if DOF == 3 % 
+      
+         %To be filled in when needed
+
+    end
+end
---
+++ Project2/MATLAB/getSTRESS.m
@@ -0,0 +1,41 @@
+function [STRESS, FORCES] = getSTRESS(SCTR,NODES,YOUNG,DOF,UGLOBAL,KGLOBAL, AREA, )
+
+    % % % This function returns the stress in a spring/bar element
+    % % % This function receives the elemental connectivity matrix, nodal
+    % % % locations, elastic modulus, degree of freedom, and solved global
+    % % % displacements vector.
+
+    % % % This function returns an array/vector that contains the elemental
+    % % % stress information for each element.
+    
+    % Create matrix to contain the position of the nodes at equilibrium
+    NEWNODES = zeros(size(NODES));
+    for i = 1:size(NODES,1)
+        for j = 1:DOF
+            NEWNODES(i,j) = NODES(i,j) + UGLOBAL(DOF*(i-1)+j);
+        end
+    end
+    
+    % Create vectors to contain the initial length of each node and the 
+    % length at equilibrium
+    initial_length = zeros(size(SCTR,1));
+    new_length = zeros(size(SCTR,1));
+    
+    % Calculate lengths
+    initial_sum_of_squares = 0;
+    for i = 1:DOF
+        initial_sum_of_squares = initial_sum_of_squares + (NODES(SCTR(:,1),i)-NODES(SCTR(:,2),i)).^2;
+    end
+    initial_length = sqrt(initial_sum_of_squares);
+
+    final_sum_of_squares = 0;
+    for i = 1:DOF
+        final_sum_of_squares = final_sum_of_squares + (NEWNODES(SCTR(:,1),i)-NEWNODES(SCTR(:,2),i)).^2;
+    end
+    new_length = sqrt(final_sum_of_squares);
+    
+    % Calculate stress
+    STRESS = ((new_length - initial_length)./initial_length).*YOUNG;
+    
+
+end
